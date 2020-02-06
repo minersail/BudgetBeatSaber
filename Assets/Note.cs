f7eccbb40aebb5f304aae01c53974272efe9176e
Phong Nguyen <42209330+Shieroz@users.noreply.github.com>
2020-02-06 17:53:17 -0500

    Cube Spawner

---
+++ Assets/Note.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float speed;
+    public float range; //How far it should go before self-destruct
+    float currentDistance; //How far the cube has moved from it's initial point
+    Vector3 initPosition; //Initial position of the note
+    Vector3 moveDirection;
+
+    public void initNote(float speed, float range, Vector3 moveDirection) {
+        initPosition = transform.position;
+        this.speed = speed;
+        this.range = range;
+        this.moveDirection = moveDirection;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentDistance = (transform.position - initPosition).magnitude;
+        if (currentDistance >= range) {
+            Destroy(gameObject);
+        }
+        transform.position += speed * moveDirection * Time.fixedDeltaTime;
+    }
+}
